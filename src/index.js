01a5d91c7fcda06d292a08796b8d7794c9b306cd
mukundborse1 <mukundborse1398@gmail.com>
2024-03-25 12:10:15 +0530

    Utils code discussion

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@
 // import { DB_NAME } from "./constants";
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Processor is running on port, ${process.env.PORT}`);
+    })  
+})
+.catch((err) => {
+    console.log('DB Connection failed', err);
+})
 
 /*
 import express from "express";
